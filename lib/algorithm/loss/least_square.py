5de4b304ccb281b6efeb9d0181402c3c892d17eb
mxvlgrz <vumvp13@gmail.com>
2020-01-05 13:03:13 -0600

    update least square

--- lib/algorithm/loss/least_square.py
+++ lib/algorithm/loss/least_square.py
@@ -5,4 +5,12 @@ import numpy as np
 def gradient(X, Y, theta):
     # t is a n * 1 vector in which each row is inner product of corresponding row of X and theta
     t = np.dot(X, theta)
+    # return gradient of every theta
     return np.dot(np.transpose(X), Y - t)
+
+
+# value of loss function of least square, which is ||(Y - XA)||^2
+def loss(X, Y, theta):
+    delta = Y - np.dot(X, theta)
+    delta_square = delta * delta
+    return sum(delta_square)
