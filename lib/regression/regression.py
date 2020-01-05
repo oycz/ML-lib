a11b4eefeb1b4d3752a2eb1813967fdaa8cc155e
mxvlgrz <vumvp13@gmail.com>
2020-01-05 13:03:59 -0600

    update regression

--- lib/regression/regression.py
+++ lib/regression/regression.py
@@ -1,10 +1,11 @@
 class Regression:
-    def __init__(self, data, training_method, training_args, classifying_method, classifying_args):
+    def __init__(self, X, Y, training_method, classifying_method, training_args, classifying_args):
         self.training_method = training_method
         self.training_args = training_args
         self.classifying_method = classifying_method
         self.classifying_args = classifying_args
-        self.data = data
+        self.X = X
+        self.Y = Y
         self.__train__()
 
     def __train__(self):
