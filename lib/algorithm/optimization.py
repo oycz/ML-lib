7b624079afeb805c18ea695d08278b4c1ca49fe2
mxvlgrz <vumvp13@gmail.com>
2020-01-04 23:48:27 -0600

    update optimization methods

--- lib/algorithm/optimization.py
+++ lib/algorithm/optimization.py
@@ -4,3 +4,7 @@ def gradient_descent(X, Y, theta, gradient_method):
     optim_theta = numpy.copy(theta)
     gradient = gradient_method(X, Y, theta)
 
+
+def stochastic_gradient_descent(X, Y, theta, gradient_method):
+    optim_theta = numpy.copy(theta)
+    gradient = gradient_method(X, Y, theta)
