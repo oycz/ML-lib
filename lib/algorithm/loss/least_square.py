04bf1e88137985d53f81bedcba96b24ec3e4ae37
mxvlgrz <vumvp13@gmail.com>
2020-01-04 23:25:12 -0600

    gradient of least square

--- lib/algorithm/loss/least_square.py
+++ lib/algorithm/loss/least_square.py
@@ -3,4 +3,6 @@ import numpy as np
 
 # gradient of least square loss function
 def gradient(X, Y, theta):
-    return np.dot(np.dot(X, theta) - Y, X)
+    # t is a n * 1 vector in which each row is inner product of corresponding row of X and theta
+    t = np.dot(X, theta)
+    return np.dot(np.transpose(X), Y - t)
---
+++ lib/regression/linear_regression.py
@@ -0,0 +1,17 @@
+import numpy as np
+import lib.algorithm.loss.least_square as loss
+from lib.regression.regression import Regression
+
+
+class LinearRegreesion(Regression):
+    def __init__(self):
+        super.__init__()
+
+
+def linear_training(X, Y, training_args):
+    initial_theta = np.zeros(Y.size)
+    gradient = loss.gradient(X, Y, initial_theta)
+
+
+def linear_predict(data, theta, classifying_args):
+    return np.dot(data, theta)
