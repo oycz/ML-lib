f76f401bba6f6d5898f726245a2cd6f2e73dc472
mxvlgrz <vumvp13@gmail.com>
2020-05-18 13:55:04 -0500

    new algorithms

---
+++ lib/classifier/gaussian_naive_bayes.py
@@ -0,0 +1,84 @@
+import numpy as np
+from collections import Counter
+from scipy import stats
+
+
+def read_file(filename, positive=2):
+    data = open(filename, "r+", encoding="UTF-8-sig")
+    X, Y = [], []
+    for line in data:
+        arr_line = line.split("\n")[0].split(",")
+        X_unit = arr_line[0:-1]
+        X_unit = [float(n) for n in X_unit]
+        Y_unit = float(arr_line[-1])
+        # correct Y_unit
+        if Y_unit == positive:
+            Y_unit = 1
+        else:
+            Y_unit = -1
+        X += [X_unit]
+        Y += [Y_unit]
+    X = np.array(X)
+    Y = np.array(Y)
+    return X, Y
+
+
+class GaussianNaiveBayes:
+
+    def __init__(self, X, Y):
+        self.X = X
+        self.Y = Y
+        self.m = X.shape[0]
+        self.n = X.shape[1]
+        self.mus = dict()
+        self.sigma2s = dict()
+        self.ys = [y[0] for y in Counter(self.Y).most_common()]
+        self.fit()
+
+    def fit(self):
+        for y in self.ys:
+            self.calc_mu(y)
+            self.calc_sigma2(y)
+
+    def calc_mu(self, y):
+        self.mus[y] = np.zeros(self.n)
+        X = self.X[self.Y == y]
+        for j in range(self.n):
+            self.mus[y][j] = np.mean(X[:, j])
+
+    def calc_sigma2(self, y):
+        self.sigma2s[y] = np.zeros(self.n)
+        X = self.X[self.Y == y]
+        for j in range(self.n):
+            muj = self.mus[y][j]
+            self.sigma2s[y][j] = np.mean((X[:, j]-muj)**2)
+
+    def predict(self, X):
+        predicts = np.zeros(X.shape[0])
+        for i in range(X.shape[0]):
+            predicts[i] = self.predict_unit(X[i])
+        return predicts
+
+    def predict_unit(self, x):
+        y_log_probs = []
+        for y in self.ys:
+            y_log_prob = 0.0
+            for j in range(self.n):
+                xj = x[j]
+                # prob = stats.norm.pdf(xj, self.mus[y][j], self.sigma2s[y][j])
+                prob = stats.norm.pdf(xj, self.mus[y][j], np.sqrt(self.sigma2s[y][j]))
+                y_log_prob += np.log(prob)
+            y_log_probs += [y_log_prob]
+        return self.ys[np.argmax(y_log_probs)]
+
+
+if __name__ == "__main__":
+    train_X, train_Y = read_file("train.data")
+    valid_X, valid_Y = read_file("valid.data")
+    test_X, test_Y = read_file("test.data")
+
+    gnb = GaussianNaiveBayes(train_X, train_Y)
+    pred_valid_Y = gnb.predict(valid_X)
+    prev_test_Y = gnb.predict(test_X)
+    accu1 = np.sum(pred_valid_Y == valid_Y) / valid_Y.size
+    accu2 = np.sum(prev_test_Y == test_Y) / test_Y.size
---
+++ lib/classifier/logistic_regression.py
@@ -0,0 +1,148 @@
+import numpy as np
+from collections import Counter
+
+
+def read_file(filename, positive=2):
+    data = open(filename, "r+", encoding="UTF-8-sig")
+    X, Y = [], []
+    for line in data:
+        arr_line = line.split("\n")[0].split(",")
+        X_unit = arr_line[0:-1]
+        X_unit = [float(n) for n in X_unit]
+        Y_unit = float(arr_line[-1])
+        if Y_unit == positive:
+            Y_unit = -1
+        else:
+            Y_unit = 1
+        X += [X_unit]
+        Y += [Y_unit]
+    X = np.array(X)
+    Y = np.array(Y)
+    return X, Y
+
+
+def none_penalty(lr, lmda):
+    def penalty():
+        return np.zeros(lr.W.shape)
+    return penalty
+
+
+def l1_penalty(lr, lmda):
+    def penalty():
+        return -lmda * np.sign(lr.W)
+    return penalty
+
+
+def l2_penalty(lr, lmda):
+    def penalty():
+        return -lmda * lr.W
+    return penalty
+
+
+class LogisticRegression:
+    def __init__(self, X, Y, lr=1, threshold=1e-8, max_iteration=float('inf'), penalty="None", lmda=1, diminishing=False):
+        self.X = X
+        self.Y = Y
+        self.n = X.shape[1]
+        self.m = X.shape[0]
+        self.W = np.zeros(self.n)
+        self.B = 0
+        self.lr = lr
+        self.iteration_time = 0
+        self.threshold = threshold
+        self.max_iteration = max_iteration
+        self.diminishing = diminishing
+        if penalty == "None":
+            self.penalty = none_penalty(self, lmda)
+        elif penalty == "l1":
+            self.penalty = l1_penalty(self, lmda)
+        elif penalty == "l2":
+            self.penalty = l2_penalty(self, lmda)
+        self.fit()
+
+    def fit(self):
+        prev_loss = -self.log_mle()
+        while True:
+            if self.iteration_time >= self.max_iteration:
+                break
+            grad_W, grad_B = self.gradient()
+            if self.diminishing:
+                self.lr = 2 / (2 + self.iteration_time)
+            self.W += self.lr * grad_W
+            self.B += self.lr * grad_B
+            self.iteration_time += 1
+            loss = -self.log_mle()
+            if np.abs(prev_loss - loss) < self.threshold:
+                break
+            prev_loss = loss
+
+    def gradient(self):
+        grad_W = self.penalty()
+        grad_B = 0
+        for i in range(self.m):
+            x = self.X[i]
+            y = self.Y[i]
+            prob_x = self.prob(x)
+            # update W
+            for j in range(self.n):
+                xj = x[j]
+                grad_W[j] += xj * ((y+1)/2 - prob_x)
+            # update B
+            grad_B += ((y+1)/2 - prob_x)
+        return grad_W, grad_B
+
+    def predict(self, X):
+        result = np.zeros(X.shape[0])
+        for i in range(X.shape[0]):
+            x = X[i]
+            prob_x = self.prob(x)
+            result[i] = 1 if prob_x >= 0.5 else -1
+        return result
+
+    def projection(self, x):
+        return np.inner(self.W, x) + self.B
+
+    def prob(self, x):
+        proj = self.projection(x)
+        return np.exp(proj) / (1 + np.exp(proj))
+
+    def log_mle(self):
+        result = 0.0
+        for i in range(self.X.shape[0]):
+            xi = self.X[i]
+            yi = self.Y[i]
+            proj = self.projection(xi)
+            result += ((yi+1) / 2)*proj - np.log(1+np.exp(proj))
+        return result
+
+
+if __name__ == "__main__":
+    train_X, train_Y = read_file("train.data")
+    valid_X, valid_Y = read_file("valid.data")
+    test_X, test_Y = read_file("test.data")
+
+    # lr = LogisticRegression(train_X, train_Y, penalty="None", lr=0.1, diminishing=False, threshold=0)
+
+    lrs = [0.001, 0.01, 0.1, 1]
+    lmdas = [0.0001, 0.001, 0.01, 0.1, 0.5, 1, 10]
+    los1 = dict()
+    accus1 = dict()
+    for lrate in lrs:
+        for lmda in lmdas:
+            lr = LogisticRegression(train_X, train_Y, penalty="l1", lmda=lmda, lr=lrate, diminishing=True, threshold=0.0001)
+            predict_valid_Y = lr.predict(valid_X)
+            los1[str(lrate), str(lmda)] = lr
+            accus1[str(lrate), str(lmda)] = [np.sum(predict_valid_Y == valid_Y) / valid_Y.size]
+            print("lr1: " + str(accus1) + ", lmda1: " + str(lmda))
+
+    lmdas = [0.0001, 0.001, 0.01, 0.1, 0.5, 1, 10]
+    lrs2 = [0.001, 0.01, 0.1, 1]
+    los2 = dict()
+    accus2 = []
+    for lrate in lrs:
+        for lmda in lmdas:
+            lr = LogisticRegression(train_X, train_Y, penalty="l2", lmda=lmda, lr=lrate, diminishing=False, threshold=0.0001)
+            predict_valid_Y = lr.predict(valid_X)
+            los2[str(lrate), str(lmda)] = lr
+            accus2[str(lrate), str(lmda)] = [np.sum(predict_valid_Y == valid_Y) / valid_Y.size]
+            print("lr2: " + str(accus1) + ", lmda2: " + str(lmda))
